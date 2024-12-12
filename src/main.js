0074721e098c36cf976fbf6e91dd8b0805b07348
Lisu02 <terminatorektt8000@gmail.com>
2024-12-12 01:38:15 +0100

    aws-test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+const port = 10000;
 const app = createApp(App);
 
 app.use(router);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example listening on port ${port}`);
+});
 
 app.mount("#app");
--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AddTaskComp from "@/components/AddTaskComp.vue";
-import TaskListing from "@/components/TaskListing.vue";
-import TaskListings from "@/components/TaskListings.vue";
+
 </script>
 
 <template>
